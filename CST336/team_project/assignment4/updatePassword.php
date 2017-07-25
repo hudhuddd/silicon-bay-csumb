40c1bff5458b50f41c4af30fb4573c520705694f
Andrea Wieland <ImFromHereAndrea@users.noreply.github.com>
2017-07-25 16:49:30 -0700

    fixed updating

--- CST336/team_project/assignment4/updatePassword.php
+++ CST336/team_project/assignment4/updatePassword.php
@@ -1,41 +1,28 @@
 <?
 require 'db_connection.php';//change this to your own database authentication file
 
-function getId($id){
-global $dbConn;
-
-$sql = "SELECT * 
-FROM movie_admin
-WHERE id = :id";
-$stmt = $dbConn -> prepare($sql);
-$stmt -> execute(array(":id"=>$id));
-return $stmt->fetch(); 
-}
-
 if (isset($_POST['save'])) { //checks whether we're coming from "save data" form
-
+$newpw = $_POST['password'];
 $sql = "UPDATE movie_admin
 SET password = :password
 WHERE id = :id";
 $stmt = $dbConn -> prepare($sql);
-$stmt -> execute(array(":id"=>$_POST['id'],
-":password"=>$_POST['password']
-));  
+$stmt -> execute(array(":password"=> hash('sha1', $newpw), ":id"=>'1'));
 
-//":id"->$_POST['id']
-
-echo "RECORD UPDATED!! <br> <br>"; 
+echo "RECORD UPDATED!! <br> <br>";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="utf-8">
 
-<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
 Remove this if you use the .htaccess -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link href="styles.css" rel="stylesheet">
 
 <title>Update Password</title>
 
@@ -47,14 +34,16 @@ Remove this if you use the .htaccess -->
 <?
 
 if (isset($_POST['id'])) {
-$userInfo = getId($_POST['id']); ?>
+  /*$userInfo = getId($_POST['id']);*/
 
-<form method="post">
-New Password: <input type="text" name="password" value="<?=$userInfo['password']?>" /><br />
-<input type="submit" name="save" value="Save"> 
-</form>
+  ?>
 
-<? }
+  <form method="post">
+  New Password: <input type="text" name="password" /><br />
+  <input type="submit" name="save" value="Save">
+  </form>
+<?
+}
 
 ?>
 <br /><br />
